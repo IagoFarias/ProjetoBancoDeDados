ed16460a96a51065f96f0d1c747e2361902a6282
IagoFarias <44845338+IagoFarias@users.noreply.github.com>
2019-06-21 11:35:29 -0300

    FrmCliente,FrmFuncionario

--- Lanchonete/src/br/com/bd2/lanchonete/view/FrmCliente.java
+++ Lanchonete/src/br/com/bd2/lanchonete/view/FrmCliente.java
@@ -9,6 +9,7 @@ import javax.swing.border.EmptyBorder;
 import javax.swing.GroupLayout;
 import javax.swing.GroupLayout.Alignment;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.LayoutStyle.ComponentPlacement;
 import javax.swing.JTextField;
 import javax.swing.JButton;
@@ -57,7 +58,7 @@ public class FrmCliente extends JFrame {
 	 */
 	public FrmCliente() {
 		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		setBounds(100, 100, 569, 413);
+		setBounds(100, 100, 703, 413);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
@@ -86,14 +87,16 @@ public class FrmCliente extends JFrame {
 
 		JScrollPane scrollPane = new JScrollPane();
 		GroupLayout gl_panel_2 = new GroupLayout(panel_2);
-		gl_panel_2.setHorizontalGroup(gl_panel_2.createParallelGroup(Alignment.LEADING)
+		gl_panel_2.setHorizontalGroup(
+			gl_panel_2.createParallelGroup(Alignment.LEADING)
+				.addComponent(scrollPane, GroupLayout.DEFAULT_SIZE, 684, Short.MAX_VALUE)
+		);
+		gl_panel_2.setVerticalGroup(
+			gl_panel_2.createParallelGroup(Alignment.LEADING)
 				.addGroup(gl_panel_2.createSequentialGroup()
-						.addComponent(scrollPane, GroupLayout.PREFERRED_SIZE, 532, GroupLayout.PREFERRED_SIZE)
-						.addContainerGap(GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)));
-		gl_panel_2.setVerticalGroup(gl_panel_2.createParallelGroup(Alignment.LEADING)
-				.addGroup(gl_panel_2.createSequentialGroup()
-						.addComponent(scrollPane, GroupLayout.PREFERRED_SIZE, 158, GroupLayout.PREFERRED_SIZE)
-						.addContainerGap(GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)));
+					.addComponent(scrollPane, GroupLayout.PREFERRED_SIZE, 158, GroupLayout.PREFERRED_SIZE)
+					.addContainerGap(GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+		);
 
 		tblConsulta = new JTable();
 		tblConsulta.addMouseListener(new MouseAdapter() {
@@ -149,13 +152,49 @@ public class FrmCliente extends JFrame {
 		});
 
 		JButton btnAtualizarDados = new JButton("Atualizar Dados");
+		btnAtualizarDados.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				Cliente cli = new Cliente();
+				ClienteController cliCon = new ClienteController();
+				cli.setNome(txtNome.getText());
+				cli.setCpf(txtCpf.getText());
+				cli.setContato(txtContato.getText());
+				cli.setEndereco(txtEndereco.getText());
+				
+				cliCon.alterar(cli);
+				
+			}
+		});
 
 		JButton btnExcluir = new JButton("Excluir");
+		btnExcluir.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				ClienteController cliCon = new ClienteController();
+				
+				
+				Cliente cli =cliCon.pesquisarPorCpf(txtCpf.getText());
+				
+				if(cli == null)
+				{
+					JOptionPane.showMessageDialog(null,"Cliente Não Encontrado!");
+					
+				}
+				else {
+					Object[] opcoes = { "Sim", "Não" };
+					int i = JOptionPane.showOptionDialog(null, "Deseja excluir esse Cliente: " + txtNome.getText() + "?",
+							"Exclusão", JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE, null, opcoes, opcoes[0]);
+					if (JOptionPane.YES_OPTION == i) {
+						cliCon.excluir(cli);
+					}
+				}
+	
+			}
+		});
 
 		JButton btnPesquisar = new JButton("Pesquisar");
 		btnPesquisar.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				List<Cliente> listaCli = new ArrayList();
+				List<Cliente> listaCli = new ArrayList<Cliente>();
 				ClienteController cliCon = new ClienteController();
 				
 				listaCli = cliCon.listarTodos();
@@ -178,20 +217,80 @@ public class FrmCliente extends JFrame {
 		});
 
 		JButton btnBuscarPorCpf = new JButton("Buscar Por CPF");
+		btnBuscarPorCpf.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				ClienteController cliCon = new ClienteController();
+				Cliente cli = cliCon.pesquisarPorCpf(txtCpf.getText());
+				
+				if(cli == null)
+				{
+					JOptionPane.showMessageDialog(null,"Cliente Não Encontrado!");
+					
+				}
+				else {
+				txtNome.setText(cli.getNome());
+				txtCpf.setText(cli.getCpf());
+				txtContato.setText(cli.getContato());
+				txtEndereco.setText(cli.getEndereco());
+				}
+			}
+		});
+		
+		JButton btnLimpar = new JButton("Limpar");
+		btnLimpar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				txtNome.setText("");
+				txtCpf.setText("");
+				txtContato.setText("");
+				txtEndereco.setText("");
+				
+				DefaultTableModel tbm = (DefaultTableModel)tblConsulta.getModel();
+				for(int i = tbm.getRowCount()-1; i >= 0; i--){
+				tbm.removeRow(i);
+				}
+				
+			}
+		});
+		
+		JButton btnSair = new JButton("Sair");
+		btnSair.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				FrmCliente.this.dispose();
+			}
+		});
 		GroupLayout gl_panel_1 = new GroupLayout(panel_1);
-		gl_panel_1.setHorizontalGroup(gl_panel_1.createParallelGroup(Alignment.LEADING)
-				.addGroup(gl_panel_1.createSequentialGroup().addComponent(btnCadastrar)
-						.addPreferredGap(ComponentPlacement.RELATED).addComponent(btnAtualizarDados)
-						.addPreferredGap(ComponentPlacement.RELATED).addComponent(btnExcluir)
-						.addPreferredGap(ComponentPlacement.RELATED).addComponent(btnPesquisar)
-						.addPreferredGap(ComponentPlacement.RELATED).addComponent(btnBuscarPorCpf)
-						.addContainerGap(78, Short.MAX_VALUE)));
-		gl_panel_1.setVerticalGroup(gl_panel_1.createParallelGroup(Alignment.LEADING)
-				.addGroup(gl_panel_1.createSequentialGroup().addContainerGap()
-						.addGroup(gl_panel_1.createParallelGroup(Alignment.BASELINE).addComponent(btnCadastrar)
-								.addComponent(btnAtualizarDados).addComponent(btnExcluir).addComponent(btnPesquisar)
-								.addComponent(btnBuscarPorCpf))
-						.addContainerGap(12, Short.MAX_VALUE)));
+		gl_panel_1.setHorizontalGroup(
+			gl_panel_1.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_panel_1.createSequentialGroup()
+					.addComponent(btnCadastrar)
+					.addPreferredGap(ComponentPlacement.RELATED)
+					.addComponent(btnAtualizarDados)
+					.addPreferredGap(ComponentPlacement.RELATED)
+					.addComponent(btnExcluir)
+					.addPreferredGap(ComponentPlacement.RELATED)
+					.addComponent(btnPesquisar)
+					.addPreferredGap(ComponentPlacement.RELATED)
+					.addComponent(btnBuscarPorCpf)
+					.addPreferredGap(ComponentPlacement.RELATED)
+					.addComponent(btnLimpar)
+					.addPreferredGap(ComponentPlacement.RELATED)
+					.addComponent(btnSair)
+					.addContainerGap(61, Short.MAX_VALUE))
+		);
+		gl_panel_1.setVerticalGroup(
+			gl_panel_1.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_panel_1.createSequentialGroup()
+					.addContainerGap()
+					.addGroup(gl_panel_1.createParallelGroup(Alignment.BASELINE)
+						.addComponent(btnCadastrar)
+						.addComponent(btnAtualizarDados)
+						.addComponent(btnExcluir)
+						.addComponent(btnPesquisar)
+						.addComponent(btnBuscarPorCpf)
+						.addComponent(btnLimpar)
+						.addComponent(btnSair))
+					.addContainerGap(12, Short.MAX_VALUE))
+		);
 		panel_1.setLayout(gl_panel_1);
 
 		JLabel lblNome = new JLabel("Nome:");
@@ -214,35 +313,49 @@ public class FrmCliente extends JFrame {
 		txtEndereco = new JTextField();
 		txtEndereco.setColumns(10);
 		GroupLayout gl_panel = new GroupLayout(panel);
-		gl_panel.setHorizontalGroup(gl_panel.createParallelGroup(Alignment.LEADING)
-				.addGroup(gl_panel.createSequentialGroup().addGroup(gl_panel.createParallelGroup(Alignment.LEADING)
-						.addGroup(gl_panel.createSequentialGroup().addComponent(lblNome)
-								.addPreferredGap(ComponentPlacement.RELATED)
-								.addComponent(txtNome, GroupLayout.PREFERRED_SIZE, 226, GroupLayout.PREFERRED_SIZE))
-						.addGroup(gl_panel.createSequentialGroup().addComponent(lblEndereo)
-								.addPreferredGap(ComponentPlacement.RELATED)
-								.addComponent(txtEndereco, GroupLayout.PREFERRED_SIZE, 359, GroupLayout.PREFERRED_SIZE))
-						.addGroup(gl_panel.createSequentialGroup().addComponent(lblContato)
-								.addPreferredGap(ComponentPlacement.RELATED)
-								.addComponent(txtContato, GroupLayout.PREFERRED_SIZE, 129, GroupLayout.PREFERRED_SIZE))
-						.addGroup(gl_panel.createSequentialGroup().addComponent(lblCpf)
-								.addPreferredGap(ComponentPlacement.RELATED)
-								.addComponent(txtCpf, GroupLayout.PREFERRED_SIZE, 201, GroupLayout.PREFERRED_SIZE)))
-						.addContainerGap(132, Short.MAX_VALUE)));
-		gl_panel.setVerticalGroup(gl_panel.createParallelGroup(Alignment.LEADING).addGroup(gl_panel
-				.createSequentialGroup().addContainerGap()
-				.addGroup(gl_panel.createParallelGroup(Alignment.BASELINE).addComponent(lblNome).addComponent(txtNome,
-						GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
-				.addPreferredGap(ComponentPlacement.RELATED)
-				.addGroup(gl_panel.createParallelGroup(Alignment.BASELINE).addComponent(lblCpf).addComponent(txtCpf,
-						GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
-				.addPreferredGap(ComponentPlacement.RELATED)
-				.addGroup(gl_panel.createParallelGroup(Alignment.BASELINE).addComponent(lblContato).addComponent(
-						txtContato, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
-				.addPreferredGap(ComponentPlacement.RELATED)
-				.addGroup(gl_panel.createParallelGroup(Alignment.LEADING).addComponent(lblEndereo).addComponent(
-						txtEndereco, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
-				.addContainerGap(70, Short.MAX_VALUE)));
+		gl_panel.setHorizontalGroup(
+			gl_panel.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_panel.createSequentialGroup()
+					.addGroup(gl_panel.createParallelGroup(Alignment.LEADING)
+						.addGroup(gl_panel.createSequentialGroup()
+							.addComponent(lblNome)
+							.addPreferredGap(ComponentPlacement.RELATED)
+							.addComponent(txtNome, GroupLayout.PREFERRED_SIZE, 226, GroupLayout.PREFERRED_SIZE))
+						.addGroup(gl_panel.createSequentialGroup()
+							.addComponent(lblEndereo)
+							.addPreferredGap(ComponentPlacement.RELATED)
+							.addComponent(txtEndereco, GroupLayout.PREFERRED_SIZE, 359, GroupLayout.PREFERRED_SIZE))
+						.addGroup(gl_panel.createSequentialGroup()
+							.addComponent(lblContato)
+							.addPreferredGap(ComponentPlacement.RELATED)
+							.addComponent(txtContato, GroupLayout.PREFERRED_SIZE, 129, GroupLayout.PREFERRED_SIZE))
+						.addGroup(gl_panel.createSequentialGroup()
+							.addComponent(lblCpf)
+							.addPreferredGap(ComponentPlacement.RELATED)
+							.addComponent(txtCpf, GroupLayout.PREFERRED_SIZE, 201, GroupLayout.PREFERRED_SIZE)))
+					.addContainerGap(130, Short.MAX_VALUE))
+		);
+		gl_panel.setVerticalGroup(
+			gl_panel.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_panel.createSequentialGroup()
+					.addContainerGap()
+					.addGroup(gl_panel.createParallelGroup(Alignment.BASELINE)
+						.addComponent(lblNome)
+						.addComponent(txtNome, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+					.addPreferredGap(ComponentPlacement.RELATED)
+					.addGroup(gl_panel.createParallelGroup(Alignment.BASELINE)
+						.addComponent(lblCpf)
+						.addComponent(txtCpf, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+					.addPreferredGap(ComponentPlacement.RELATED)
+					.addGroup(gl_panel.createParallelGroup(Alignment.BASELINE)
+						.addComponent(lblContato)
+						.addComponent(txtContato, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+					.addPreferredGap(ComponentPlacement.RELATED)
+					.addGroup(gl_panel.createParallelGroup(Alignment.LEADING)
+						.addComponent(lblEndereo)
+						.addComponent(txtEndereco, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+					.addContainerGap(16, Short.MAX_VALUE))
+		);
 		panel.setLayout(gl_panel);
 		contentPane.setLayout(gl_contentPane);
 	}
--- Lanchonete/src/br/com/bd2/lanchonete/view/FrmFuncionario.java
+++ Lanchonete/src/br/com/bd2/lanchonete/view/FrmFuncionario.java
@@ -6,10 +6,36 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.LayoutStyle.ComponentPlacement;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+
+import br.com.bd2.lanchonete.controller.FuncionarioController;
+import br.com.bd2.lanchonete.negocio.Cliente;
+import br.com.bd2.lanchonete.negocio.Funcionario;
+
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.List;
+import java.awt.event.ActionEvent;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 public class FrmFuncionario extends JFrame {
 
 	private JPanel contentPane;
+	private JTextField txtNome;
+	private JTextField txtCpf;
+	private JTextField txtEndereco;
+	private JTextField txtContato;
+	private JTable tblConsulta;
 
 	/**
 	 * Launch the application.
@@ -31,12 +57,275 @@ public class FrmFuncionario extends JFrame {
 	 * Create the frame.
 	 */
 	public FrmFuncionario() {
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		setBounds(100, 100, 700, 373);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
-	}
 
+		JPanel panel = new JPanel();
+
+		JPanel panel_1 = new JPanel();
+
+		JPanel panel_2 = new JPanel();
+		GroupLayout gl_contentPane = new GroupLayout(contentPane);
+		gl_contentPane.setHorizontalGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_contentPane.createSequentialGroup()
+						.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
+								.addComponent(panel, GroupLayout.DEFAULT_SIZE, 664, Short.MAX_VALUE)
+								.addComponent(panel_1, GroupLayout.DEFAULT_SIZE, 664, Short.MAX_VALUE)
+								.addComponent(panel_2, GroupLayout.PREFERRED_SIZE, 664, Short.MAX_VALUE))
+						.addContainerGap()));
+		gl_contentPane.setVerticalGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_contentPane.createSequentialGroup()
+						.addComponent(panel, GroupLayout.PREFERRED_SIZE, 104, GroupLayout.PREFERRED_SIZE)
+						.addPreferredGap(ComponentPlacement.RELATED)
+						.addComponent(panel_1, GroupLayout.PREFERRED_SIZE, 45, GroupLayout.PREFERRED_SIZE)
+						.addPreferredGap(ComponentPlacement.RELATED)
+						.addComponent(panel_2, GroupLayout.PREFERRED_SIZE, 149, GroupLayout.PREFERRED_SIZE)
+						.addContainerGap(14, Short.MAX_VALUE)));
+
+		JLabel lblNome = new JLabel("Nome:");
+
+		JLabel lblNewLabel = new JLabel("CPF:");
+
+		JLabel lblEndereo = new JLabel("Endere\u00E7o:");
+
+		JLabel lblContato = new JLabel("Contato:");
+
+		txtNome = new JTextField();
+		txtNome.setColumns(10);
+
+		txtCpf = new JTextField();
+		txtCpf.setColumns(10);
+
+		txtEndereco = new JTextField();
+		txtEndereco.setColumns(10);
+
+		txtContato = new JTextField();
+		txtContato.setColumns(10);
+		GroupLayout gl_panel = new GroupLayout(panel);
+		gl_panel.setHorizontalGroup(gl_panel.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_panel.createSequentialGroup().addGroup(gl_panel.createParallelGroup(Alignment.LEADING)
+						.addGroup(gl_panel.createSequentialGroup().addComponent(lblNome)
+								.addPreferredGap(ComponentPlacement.RELATED)
+								.addComponent(txtNome, GroupLayout.PREFERRED_SIZE, 228, GroupLayout.PREFERRED_SIZE))
+						.addGroup(gl_panel.createSequentialGroup()
+								.addComponent(lblContato, GroupLayout.PREFERRED_SIZE, 47, GroupLayout.PREFERRED_SIZE)
+								.addPreferredGap(ComponentPlacement.RELATED)
+								.addComponent(txtContato, GroupLayout.PREFERRED_SIZE, 129, GroupLayout.PREFERRED_SIZE))
+						.addGroup(gl_panel.createSequentialGroup().addComponent(lblNewLabel)
+								.addPreferredGap(ComponentPlacement.UNRELATED)
+								.addComponent(txtCpf, GroupLayout.PREFERRED_SIZE, 150, GroupLayout.PREFERRED_SIZE))
+						.addGroup(gl_panel.createSequentialGroup().addComponent(lblEndereo)
+								.addPreferredGap(ComponentPlacement.RELATED).addComponent(txtEndereco,
+										GroupLayout.PREFERRED_SIZE, 264, GroupLayout.PREFERRED_SIZE)))
+						.addContainerGap(253, Short.MAX_VALUE)));
+		gl_panel.setVerticalGroup(gl_panel.createParallelGroup(Alignment.LEADING).addGroup(gl_panel
+				.createSequentialGroup()
+				.addGroup(gl_panel.createParallelGroup(Alignment.LEADING).addComponent(lblNome).addComponent(txtNome,
+						GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+				.addPreferredGap(ComponentPlacement.RELATED)
+				.addGroup(gl_panel.createParallelGroup(Alignment.LEADING).addComponent(lblNewLabel).addComponent(txtCpf,
+						GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+				.addPreferredGap(ComponentPlacement.RELATED)
+				.addGroup(gl_panel.createParallelGroup(Alignment.LEADING).addComponent(lblEndereo).addComponent(
+						txtEndereco, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+				.addPreferredGap(ComponentPlacement.RELATED)
+				.addGroup(gl_panel.createParallelGroup(Alignment.BASELINE).addComponent(lblContato).addComponent(
+						txtContato, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+				.addContainerGap(GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)));
+		panel.setLayout(gl_panel);
+
+		JButton btnInserir = new JButton("Cadastrar");
+		btnInserir.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				Funcionario func = new Funcionario();
+				FuncionarioController funcCO = new FuncionarioController();
+
+				func.setNome(txtNome.getText());
+				func.setCpf(txtCpf.getText());
+				func.setEndereco(txtEndereco.getText());
+				func.setContato(txtContato.getText());
+
+				funcCO.inserir(func);
+
+			}
+		});
+
+		JButton btnExcluir = new JButton("Excluir");
+		btnExcluir.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				FuncionarioController funcCO = new FuncionarioController();
+
+				Funcionario func = funcCO.pesquisarPorCpf(txtCpf.getText());
+
+				if (func == null) {
+					JOptionPane.showMessageDialog(null, "Funcionário Não Encontrado!");
+
+				} else {
+					Object[] opcoes = { "Sim", "Não" };
+					int i = JOptionPane.showOptionDialog(null,
+							"Deseja excluir esse Funcionário: " + txtNome.getText() + "?", "Exclusão",
+							JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE, null, opcoes, opcoes[0]);
+					if (JOptionPane.YES_OPTION == i) {
+						funcCO.excluir(func);
+					}
+				}
+
+			}
+		});
+
+		JButton btnAtualizarDados = new JButton("Atualizar Dados");
+		btnAtualizarDados.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				Funcionario func = new Funcionario();
+				FuncionarioController funcCO = new FuncionarioController();
+
+				func.setNome(txtNome.getText());
+				func.setCpf(txtCpf.getText());
+				func.setEndereco(txtEndereco.getText());
+				func.setContato(txtContato.getText());
+				
+				funcCO.alterar(func);
+			}
+		});
+
+		JButton btnPesquisar = new JButton("Pesquisar");
+		btnPesquisar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				List<Funcionario> listaFunc = new ArrayList<Funcionario>();
+				FuncionarioController funcCO = new FuncionarioController();
+
+				listaFunc = funcCO.listarTodos();
+
+				DefaultTableModel tbm = (DefaultTableModel) tblConsulta.getModel();
+				// Limpa a Tabela
+				for (int i = tbm.getRowCount() - 1; i >= 0; i--) {
+					tbm.removeRow(i);
+				}
+				// Preenche com os funcionários
+				int i = 0;
+				for (Funcionario func : listaFunc) {
+					tbm.addRow(new String[1]);
+					tblConsulta.setValueAt(func.getCodFuncionario(), i, 0);
+					tblConsulta.setValueAt(func.getNome(), i, 1);
+					tblConsulta.setValueAt(func.getCpf(), i, 2);
+					tblConsulta.setValueAt(func.getEndereco(), i, 3);
+					tblConsulta.setValueAt(func.getContato(), i, 4);
+					i++;
+				}
+			}
+		});
+
+		JButton btnPesquisarPorCpf = new JButton("Pesquisar Por CPF");
+		btnPesquisarPorCpf.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				FuncionarioController funcCO = new FuncionarioController();
+				Funcionario func = funcCO.pesquisarPorCpf(txtCpf.getText());
+				
+				if(func == null)
+				{
+					JOptionPane.showMessageDialog(null,"Funcionário Não Encontrado!");
+					
+				}
+				else {
+				txtNome.setText(func.getNome());
+				txtCpf.setText(func.getCpf());
+				txtContato.setText(func.getContato());
+				txtEndereco.setText(func.getEndereco());
+				}
+
+			}
+		});
+
+		JButton btnLimpar = new JButton("Limpar");
+		btnLimpar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				txtNome.setText("");
+				txtCpf.setText("");
+				txtContato.setText("");
+				txtEndereco.setText("");
+
+				DefaultTableModel tbm = (DefaultTableModel) tblConsulta.getModel();
+				for (int i = tbm.getRowCount() - 1; i >= 0; i--) {
+					tbm.removeRow(i);
+				}
+			}
+		});
+
+		JButton btnSair = new JButton("Sair");
+		btnSair.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				FrmFuncionario.this.dispose();
+			}
+		});
+		GroupLayout gl_panel_1 = new GroupLayout(panel_1);
+		gl_panel_1.setHorizontalGroup(gl_panel_1.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_panel_1.createSequentialGroup().addComponent(btnInserir)
+						.addPreferredGap(ComponentPlacement.RELATED).addComponent(btnAtualizarDados)
+						.addPreferredGap(ComponentPlacement.RELATED).addComponent(btnExcluir)
+						.addPreferredGap(ComponentPlacement.RELATED).addComponent(btnPesquisar)
+						.addPreferredGap(ComponentPlacement.RELATED).addComponent(btnPesquisarPorCpf)
+						.addPreferredGap(ComponentPlacement.RELATED).addComponent(btnLimpar)
+						.addPreferredGap(ComponentPlacement.RELATED).addComponent(btnSair)
+						.addContainerGap(27, Short.MAX_VALUE)));
+		gl_panel_1.setVerticalGroup(gl_panel_1.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_panel_1.createSequentialGroup().addContainerGap()
+						.addGroup(gl_panel_1.createParallelGroup(Alignment.BASELINE).addComponent(btnInserir)
+								.addComponent(btnAtualizarDados).addComponent(btnExcluir).addComponent(btnPesquisar)
+								.addComponent(btnPesquisarPorCpf).addComponent(btnLimpar).addComponent(btnSair))
+						.addContainerGap(GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)));
+		panel_1.setLayout(gl_panel_1);
+
+		JScrollPane scrollPane = new JScrollPane();
+		GroupLayout gl_panel_2 = new GroupLayout(panel_2);
+		gl_panel_2.setHorizontalGroup(gl_panel_2.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_panel_2.createSequentialGroup()
+						.addComponent(scrollPane, GroupLayout.PREFERRED_SIZE, 663, GroupLayout.PREFERRED_SIZE)
+						.addContainerGap(GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)));
+		gl_panel_2.setVerticalGroup(gl_panel_2.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_panel_2.createSequentialGroup()
+						.addComponent(scrollPane, GroupLayout.PREFERRED_SIZE, 150, GroupLayout.PREFERRED_SIZE)
+						.addContainerGap(GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)));
+
+		tblConsulta = new JTable();
+		tblConsulta.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				Integer linha = tblConsulta.getSelectedRow();
+				String nome = tblConsulta.getValueAt(linha, 1).toString();
+				String cpf = tblConsulta.getValueAt(linha, 2).toString();
+				String endereco = tblConsulta.getValueAt(linha, 3).toString();
+				String contato = tblConsulta.getValueAt(linha, 4).toString();
+				
+				txtNome.setText(nome);
+				txtCpf.setText(cpf);
+				txtContato.setText(contato);
+				txtEndereco.setText(endereco);
+			}
+		});
+		tblConsulta.setModel(new DefaultTableModel(new Object[][] {},
+				new String[] { "C\u00F3digo", "Nome", "CPF", "Endere\u00E7o", "Fones" }) {
+			Class[] columnTypes = new Class[] { Integer.class, String.class, String.class, String.class, String.class };
+
+			public Class getColumnClass(int columnIndex) {
+				return columnTypes[columnIndex];
+			}
+
+			boolean[] columnEditables = new boolean[] { false, false, false, false, false };
+
+			public boolean isCellEditable(int row, int column) {
+				return columnEditables[column];
+			}
+		});
+		tblConsulta.getColumnModel().getColumn(0).setResizable(false);
+		tblConsulta.getColumnModel().getColumn(1).setResizable(false);
+		tblConsulta.getColumnModel().getColumn(2).setResizable(false);
+		tblConsulta.getColumnModel().getColumn(3).setResizable(false);
+		tblConsulta.getColumnModel().getColumn(4).setResizable(false);
+		scrollPane.setViewportView(tblConsulta);
+		panel_2.setLayout(gl_panel_2);
+		contentPane.setLayout(gl_contentPane);
+	}
 }
